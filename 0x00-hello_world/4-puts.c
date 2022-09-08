618475a690187d61c09a6f909a6eb528a6c30895
Ololade <dairololade246@gmail.com>
2022-09-08 12:18:53 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
-/*Programming is like building a multilingual puzzle*/
-#include <studio.h>
-/*4-puts.c*/
+/*file:4-puts*/
+#include <stdio.h>
+/*main - prints "Programming is like building a multilingual puzzle, followed, 
+ *by a new line.
+ */
 int main(viod){
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
