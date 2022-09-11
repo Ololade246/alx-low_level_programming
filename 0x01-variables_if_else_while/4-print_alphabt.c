1b8bff165c5884524226cc96907b0dc9c09d2674
Ololade <dairololade246@gmail.com>
2022-09-11 10:33:27 -0700

    Task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print if number is positive, zero, or negative
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet;
+	for (alphabet = 'a', alphabeth = 'z', alphabet++)
+	{
+		if (alphabet != 'q' && alphabet ! = 'e')
+		{
+			putchar(alphabet);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
