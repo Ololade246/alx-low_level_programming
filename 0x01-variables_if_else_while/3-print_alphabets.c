c635680eec5e36c662853738264f29135c94ba34
Ololade <dairololade246@gmail.com>
2022-09-11 11:05:14 -0700

    Task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,10 @@
 int main(void)
 {
 	char alphabets;
-
 	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
-	putchar(alphabets);
-
+		putchar(alphabets);
 	for (alphabets = 'A'; alphabets <= 'Z'; alphabets++)
-	putchar(alphabets);
-
+		putchar(alphabets);
 	putchar(alphabets);
 	return (0);
 }
