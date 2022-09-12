6b02826b1fb1af2e1ea7c221b47a5628c4987a46
Ololade <dairololade246@gmail.com>
2022-09-12 10:51:05 -0700

    Task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,17 @@ int main(void)
 	int n1, n2;
 
 	for (n1 = 0; n1 < 10; n1++)
+	{
 		for (n2 = 0; n2 < 10; n2++)
+		{
 			putchar((n1 % 10) + '0');
-	putchar((n2 % 10) + '0');
-	if (n1 == 9 && n2 == 9)
-		continue;
-	putchar(',');
-	putchar(' ');
+			putchar((n2 % 10) + '0');
+			if (n1 == 9 && n2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
