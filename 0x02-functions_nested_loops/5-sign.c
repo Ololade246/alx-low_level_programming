371ce2a392027906cee42d5d36ad07f6b0331872
Ololade <dairololade246@gmail.com>
2022-09-13 18:35:51 -0700

    Task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - function that print the sign of a number
+ * @c: is the int that will be use for argumet of the function
+ *
+ * Return: Always 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1)'
+	}
+	return (0);
+}
