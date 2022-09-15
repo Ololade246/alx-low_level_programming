b13bd8dabdbdabcf0fdaec87dec06c206483b45b
Ololade <dairololade246@gmail.com>
2022-09-14 17:04:14 -0700

    Task13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - prints the sum of all the multiples of 3 or 5 below 1024
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	return (0);
+}
