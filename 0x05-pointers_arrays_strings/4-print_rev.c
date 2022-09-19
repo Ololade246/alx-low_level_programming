25dfe7fcf246c97797da92de9d89bd0d71b24fb0
Ololade <dairololade246@gmail.com>
2022-09-19 12:25:28 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,9 +13,9 @@ void print_rev(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; s[i] != '\0'; i++)
+		fcounter++;
 	}
-	for (i = 0; s[i] != '\0'; i++)
+	for (n = (fcounter - 1); n >= 0; n--)
 	{
 		_putchar(s[n]);
 	}
