c5f10976eaff180e5c0114def1c296d2e67a6a4e
Ololade <dairololade246@gmail.com>
2022-09-20 11:13:22 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,40 @@
 #include "main.h"
+
 /**
- * _itoi - Entry point
- * @s: input
+ * _atoi - converts the first set of digits in a string to an integer, taking into account the sign of the number
+ * @s: string to be converted
  *
- * Return: Always 0
+ * Return: int containing the converted number
  */
 int _atoi(char *s)
 {
-	return (0);
+	int index, ind2;
+	unsigned int res;
+	unsigned int res;
+	char now;
+
+	index = 0;
+	res = 0;
+	while (*(s + index) != '\0')
+	{
+		now = *(s + index);
+		if (now == '-')
+		{
+			sign *= -1;
+		}
+		if (now >= '0' && now <= '9')
+		{
+			ind2 = index;
+			while (*(s + ind2) > 47 && *(s + ind2) < 58)
+			{
+				res = (res * 10) + *(s + ind2) - '0';
+				ind2++;
+			}
+			break;
+		}
+		index++;
+	}
+	if (sign < 0)
+		res *= sign;
+	return (res);
 }
