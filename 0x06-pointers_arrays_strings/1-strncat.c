858a89660c3aadec8629f900e81b44d9f9618967
Ololade <dairololade246@gmail.com>
2022-09-22 13:25:25 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,12 +13,12 @@ char *_strncat(char *dest, char *src, int n)
 	int len1 = 0; /* string length for src */
 	int len2 = 0; /* string length for dest */
 	int i;
-
+	
 	while (src[len1] != '\0')
 		len1++;
-
 	while (dest[len2] != '\0')
 		len2++;
+	
 	for (i = 0; i < n; i++)
 	{
 		dest[len2] = src[i];
